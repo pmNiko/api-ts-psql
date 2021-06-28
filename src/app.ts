f2c58e7c994e4cff36f7f81fab0608d80ce0e956
pmNiko <nikolas090189@gmail.com>
2021-06-28 18:45:54 -0300

    First commit

---
+++ src/app.ts
@@ -0,0 +1,52 @@
+import express, { Application, json, urlencoded } from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+
+type Port = string | number
+
+export class App {
+  /**
+   * props to class
+   */
+  private app: Application
+  private port?: Port
+
+  constructor(aPort?: Port) {
+    this.app = express()
+    this.port = aPort
+    this.setting()
+    this.middlewares()
+  }
+
+  /**
+   * middlewares
+   */
+  middlewares() {
+    // Show request to terminal
+    this.app.use(morgan('dev'))
+
+    // Request cross dommain
+    this.app.use(cors())
+
+    // Read from body
+    this.app.use(json())
+
+    // Enable send data from from
+    this.app.use(urlencoded({ extended: false }))
+  }
+
+  /**
+   * asignation of port
+   */
+  setting() {
+    this.app.set('port', this.port || process.env.PORT || 4000)
+  }
+
+  /**
+   * server listen
+   */
+  async listen() {
+    await this.app.listen(this.app.get('port'))
+    console.log("Server on port: ", this.app.get('port'));
+  }
+}
